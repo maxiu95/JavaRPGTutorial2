c77d0cdd7cd8196a2416417455a6e08b92c6e8b4
Matthew Harley English <matthewenglish16@gmail.com>
2023-11-11 13:25:43 +0900

    Implemented code from second video of youtube tutorial. added all player sprites into player class as well as movement and simple animation when a movement key is pressed. also set animation speed

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
 
@@ -15,30 +18,102 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+
+        try {
+
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_up1.PNG"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_up2.PNG"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_down1.PNG"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_down2.PNG"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_left1.PNG"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_left2.PNG"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_right1.PNG"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/anpanman_right2.PNG"));
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
 
-        if(keyH.upPressed) {
-            y -= speed;
-        } else if(keyH.downPressed) {
-            y += speed;
-        } else if(keyH.leftPressed) {
-            x -= speed;
-        } else if(keyH.rightPressed) {
-            x += speed;
+        if(keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+
+            if(keyH.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if(keyH.downPressed) {
+                y += speed;
+                direction = "down";
+            } else if(keyH.leftPressed) {
+                x -= speed;
+                direction = "left";
+            } else if(keyH.rightPressed) {
+                x += speed;
+                direction = "right";
+            }
+
+            spriteCounter++;
+            if(spriteCounter > 15) {
+                if(spriteNumber == 1) {
+                    spriteNumber = 2;
+                } else if(spriteNumber ==2) {
+                    spriteNumber = 1;
+                }
+                spriteCounter = 0;
+            }
         }
     }
 
     public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.LIGHT_GRAY);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch(direction) {
+            case "up":
+                if(spriteNumber == 1) {
+                    image = up1;
+                }
+                if(spriteNumber == 2) {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNumber == 1) {
+                    image = down1;
+                }
+                if(spriteNumber == 2) {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNumber == 1) {
+                    image = left1;
+                }
+                if(spriteNumber == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNumber == 1) {
+                    image = right1;
+                }
+                if(spriteNumber == 2) {
+                    image = right2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
