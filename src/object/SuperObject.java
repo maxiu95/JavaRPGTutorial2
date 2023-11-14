356c91f6216f5ada99d3ac20d6c91b4dd7bfc93e
Matthew Harley English <matthewenglish16@gmail.com>
2023-11-14 14:12:43 +0900

    Implemented code from second video of youtube tutorial. updated collision box for objects

--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -11,7 +11,7 @@ public class SuperObject {
     public String name;
     public boolean collision = false;
     public int worldX, worldY;
-    public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+    public Rectangle solidArea = new Rectangle(6, 12, 24, 18);
     public int solidAreaDefaultX = 0;
     public int solidAreaDefaultY = 0;
 
